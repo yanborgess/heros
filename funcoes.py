e1c7ad349b3d30aaea1a7701032c61073f33cb19
yanborgess <yan.borges.areda@gmail.com>
2024-10-15 22:22:27 -0300

    Documentação

--- funcoes.py
+++ funcoes.py
@@ -1,21 +1,25 @@
 from herois import *
 
+# OBTER HEROI POR ID 
 def obter_herois_id(id: int )-> Heros:
     for heroi in herois:
         if heroi.id == id:
             return heroi 
-        
+
+# EDITAR HEROI
 def editar_heroi (id, heroi_alterado: Heros) -> dict:
     for indice, heroi in enumerate(herois):
         if heroi.id == id:
             herois[indice]= heroi_alterado
             return heroi
         
+# INCLUIR NOVO HEROI
 def incluir_novo_heroi(novo_heroi:Heros):
     novo_heroi.id = len(herois) + 1
     herois.append(novo_heroi)
     return novo_heroi
 
+# DELETAR HEROI
 def deletar_heroi(id):
     for indice, heroi in enumerate(herois):
         if heroi.id == id:
@@ -23,6 +27,7 @@ def deletar_heroi(id):
             return {"message": "Heroi excluído com sucesso"}
     return {}
 
+# EDITAR PODER DO HEROI 
 def editar_poderes(id, novos_poder: str):
     for indice, heroi in enumerate(herois):
         if heroi.id == id:
